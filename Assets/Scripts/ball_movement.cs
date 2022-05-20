7f224c1dff57621f8c4accb5ecb844529ed53564
Hammas <66756928+hammas1002@users.noreply.github.com>
2022-05-20 22:07:14 +0500

    Initial commit

---
+++ Assets/Scripts/ball_movement.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ball_movement : MonoBehaviour
+{
+    private Rigidbody2D ball;
+    public float speed;
+    public Vector2 direction;
+    private AudioSource audioSource;
+    public AudioSource gameMusic;
+    public AudioClip hitSound;
+    public AudioClip dieSound;
+    public Text score_text;
+    public Animator scoreAnimator;
+    private player _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GetComponent<Rigidbody2D>();
+        audioSource = GetComponent<AudioSource>();
+        _player = GameObject.Find("Player").GetComponent<player>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        ball.velocity = new Vector2(direction.x, direction.y) * speed * Time.deltaTime;
+
+    }
+    private void Update()
+    {
+        if(transform.position.x< -9.25f|| transform.position.x > 9.25f|| transform.position.y < -5.27f|| transform.position.y > 5.27f)
+        {
+            death();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name=="Paddle Floor")
+        {
+            direction.y *= -1;
+        }
+        else if (collision.name == "Paddle Wall")
+        {
+            direction.x *= -1;
+        }
+        hitEffect();
+    }
+
+    void hitEffect()
+    {
+        _player.playerScore += 1;
+
+        audioSource.pitch = Random.Range(0.8f,1.3f);
+        audioSource.PlayOneShot(hitSound);
+
+        scoreAnimator.SetTrigger("effect");
+
+        score_text.text = _player.playerScore.ToString();
+        
+    }
+    void death()
+    {
+        gameMusic.Stop();
+        transform.position = Vector2.zero;
+        audioSource.PlayOneShot(dieSound);
+        _player.playerScore =0;
+        scoreAnimator.SetTrigger("effect");
+        
+        score_text.text = "Oh No!";
+        gameMusic.Play();
+    }
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Camera camera2D;
+    public LayerMask layerMaskFloor;
+    public LayerMask layerMaskWall;
+    public GameObject paddleFloor;
+    public GameObject paddleWall;
+    public int playerScore;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction =camera2D.ScreenToWorldPoint(Input.mousePosition)-transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle,Vector3.forward);
+        transform.rotation = rotation;
+
+        RaycastHit2D hitFloor = Physics2D.Raycast(transform.position, transform.right,20,layerMaskFloor);
+        Debug.DrawRay(transform.position,transform.right*20, Color.green);
+
+
+        if (hitFloor.collider != null)
+        {
+            if (hitFloor.point.y<0)
+            {
+                //move to floor
+                paddleFloor.transform.position = new Vector2(hitFloor.point.x,-4.78f);
+                
+            }
+            else
+            {
+                //move to ceiling
+                paddleFloor.transform.position = new Vector2(hitFloor.point.x, 4.78f);
+            }
+        }
+
+        RaycastHit2D hitWalls = Physics2D.Raycast(transform.position, transform.right, 20, layerMaskWall);
+        Debug.DrawRay(transform.position, transform.right * 20, Color.green);
+
+        if (hitWalls.collider != null)
+        {
+            if (hitWalls.point.x < 0)
+            {
+                //move to floor
+                paddleWall.transform.position = new Vector2(-8.59f, hitWalls.point.y);
+
+            }
+            else
+            {
+                //move to ceiling
+                paddleWall.transform.position = new Vector2(8.59f, hitWalls.point.y);
+            }
+        }
+
+    }
+}
